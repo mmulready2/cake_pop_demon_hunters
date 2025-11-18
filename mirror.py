72f5b9e96517ebed7c6a254f009c6dbd360af185
mmulready2 <mmulready2@mail.niagara.edu>
2025-11-17 20:15:55 -0500

    Add files via upload
    Added face detection and tracking

--- mirror.py
+++ mirror.py
@@ -2,6 +2,7 @@ print("This is mirror.py")
 
 import numpy as np
 import cv2 as cv
+import time, random
 
 cap = cv.VideoCapture(0)
 
@@ -9,13 +10,18 @@ if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
+# Face detection
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+
 first = True
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     # turn the image into mirror image
     frame = cv.flip(frame,1)
-    height, width = frame.shape[:2]
+    height = frame.shape[0]
+    width  = frame.shape[1]
 
     # if frame is read correctly ret is True
     if not ret:
@@ -27,13 +33,33 @@ while True:
         prev_gray = gray
         first = False
 
+    # More face detection
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        break
+
     # Calculate change in gray
     delta = cv.absdiff(gray[0:100],prev_gray[0:100]).sum()
-    cv.rectangle(frame, (0,0), (100,100),(100,100,100),2)
+    # Top Left
+    cv.rectangle(frame, (0, 0), (300, 300), (100,100,100), 2)
+    # Top Right
+    cv.rectangle(frame, (1500, 50), (1800, 350), (100,100,100), 2)
+    # Bottom Left
+    cv.rectangle(frame, (0, 1300), (300, 1600), (100,100,100), 2)
+    # Bottom Right
+    cv.rectangle(frame, (1500, 750), (1800, 1050), (100,100,100), 2)
 
     # Display text
     cv.putText(frame, str(delta), (50,300), cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 5)
+    # Top Left
     cv.putText(frame, "Demon", (100,100), cv.FONT_HERSHEY_SIMPLEX, 2, (150,0,150), 5)
+    # Top Right
+    cv.putText(frame, "Demon", (1600,100), cv.FONT_HERSHEY_SIMPLEX, 2, (150,0,150), 5)
+    # Bottom Left
+    cv.putText(frame, "Demon", (100,1000), cv.FONT_HERSHEY_SIMPLEX, 2, (150,0,150), 5)
+    # Bottom Right
+    cv.putText(frame, "Demon", (1600,1000), cv.FONT_HERSHEY_SIMPLEX, 2, (150,0,150), 5)
 
     # Display resulting frame
     # cv.imshow('frame', gray)
